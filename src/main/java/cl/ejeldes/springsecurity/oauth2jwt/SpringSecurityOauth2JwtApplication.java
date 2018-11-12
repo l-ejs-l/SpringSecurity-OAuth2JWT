c6088163098f7a90e9191b9bcc331d0e507cedde
emilio <emilio.jeldes@gmail.com>
2018-11-12 09:55:07 -0300

    first commit

---
+++ src/main/java/cl/ejeldes/springsecurity/oauth2jwt/SpringSecurityOauth2JwtApplication.java
@@ -0,0 +1,12 @@
+package cl.ejeldes.springsecurity.oauth2jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2JwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOauth2JwtApplication.class, args);
+    }
+}
